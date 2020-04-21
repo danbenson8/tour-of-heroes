c1683b303e84d1fed2264f95e4986aec2a74a1f9
danbenson8 <danbenson8@gmail.com>
2020-04-21 16:18:23 +0100

    add getHeroes function incl. error handling and message logging.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
-import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
     providedIn: 'root'
@@ -17,20 +19,44 @@ export class HeroService {
     private log(message: string) {
         this.messageService.add(`HeroService: ${message}`);
     }
-    
+
     private heroesUrl = 'api/heroes' // Url to web API
+
+    /**
+    * Handle Http operation that failed.
+    * Let the app continue.
+    * @param operation - name of the operation that failed
+    * @param result - optional value to return as the observable result
+    */
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            // TODO: better job of transforming error for user consumption
+            this.log(`${operation} failed: ${error.message}`);
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
 
     getHeroes(): Observable<Hero[]> {
         // TODO send the message _after_ fetching heroes
         this.messageService.add('HeroService: fetched heroes');
-        return this.http.get<Hero[]>(this.heroesUrl);
+        return this.http.get<Hero[]>(this.heroesUrl)
+            .pipe(
+                tap(_ => this.log('fetched heroes')),
+                catchError(this.handleError<Hero[]>('getHeroes', []))
+            );
     }
 
     // TODO fix this
     getHero(id: number): Observable<Hero> {
         // TODO send the message _after_ fetching heroes
         this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of();
+        return;
     }
 
 
