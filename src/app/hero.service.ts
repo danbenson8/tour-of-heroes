98b7fc928e8e34d06fa366ffe73e588eca4b6ebe
danbenson8 <danbenson8@gmail.com>
2020-04-21 16:01:10 +0100

    modify to extract heroes from in memory data api.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HeroService {
 
-    constructor(private messageService: MessageService) { }
-    
-    getHeroes(): Observable<Hero[]> {
-        // TODO send the message _after_ fetching heroes
-        this.messageService.add('HeroService: fetched heroes'); 
-        return of(HEROES);
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService) { }
+
+    /** Log a HeroService message with the MessageService */
+    private log(message: string) {
+        this.messageService.add(`HeroService: ${message}`);
     }
     
+    private heroesUrl = 'api/heroes' // Url to web API
+
+    getHeroes(): Observable<Hero[]> {
+        // TODO send the message _after_ fetching heroes
+        this.messageService.add('HeroService: fetched heroes');
+        return this.http.get<Hero[]>(this.heroesUrl);
+    }
+
+    // TODO fix this
     getHero(id: number): Observable<Hero> {
         // TODO send the message _after_ fetching heroes
         this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of(HEROES.find(hero => hero.id === id));
-    } 
+        return of();
+    }
+
 
 }
