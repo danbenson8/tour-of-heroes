3fa8630fc8627ac5428a248a0d7c91b771f24a9a
danbenson8 <danbenson8@gmail.com>
2020-04-21 14:06:30 +0100

    add message functionality: fetching heroes, selecting hero & clear messages.

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MessageService {
 
-constructor() { }
+    messages: string[] = [];
+
+    constructor() { }
+
+    add(message: string) {
+        this.messages.push(message);
+    }
+
+    clear() {
+        this.messages = [];
+    }
 
 }
